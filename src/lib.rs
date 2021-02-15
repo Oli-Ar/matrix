8c9983b0dea95199d20f7151f67904aea17e49da
Oliver <oliver@archer.fi>
2021-02-15 23:43:22 +0000

    Implemented working matrix implementation as well as implementing the display trait for the matrices

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ use proc_macro::TokenStream;
 use quote::quote;
 use syn::{
     parse::{Parse, ParseStream},
+    parse_macro_input,
     spanned::Spanned,
     Error, Expr, ExprArray, Lit, Result,
 };
@@ -90,6 +91,33 @@ fn check_valid_input(array: &ExprArray) -> Result<proc_macro2::TokenStream> {
 }
 
 #[proc_macro]
-pub fn matrix(_input: TokenStream) -> TokenStream {
-    TokenStream::new()
+pub fn matrix(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as MatrixInput);
+    let output_len = input.dimensions.0 * input.dimensions.1;
+    let output_arr = input.parsed_arr;
+
+    let x_dim = input.dimensions.0;
+    let y_dim = input.dimensions.1;
+
+    TokenStream::from(quote! {{
+        use std::fmt::{Display, Formatter, Result};
+        #[derive(Debug)]
+        pub struct Matrix([i32; #output_len]);
+
+        // Prints the user inputted array in the same dimensions as the passed in
+        impl Display for Matrix {
+            fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+                for i in 0..#x_dim {
+                    if i == 0 { write!(f, "[")? } else { write!(f, " ")? };
+                    for j in 0..#y_dim {
+                        write!(f, "{:>3},", self.0[#y_dim*i+j])?;
+                    }
+                    if i == #x_dim-1 { write!(f, "]")? } else { write!(f, "\n")? };
+                }
+                Ok(())
+            }
+        }
+
+        Matrix(#output_arr)
+    }})
 }
