87d75870d497f7472493fd7d1184b74e95e75118
Oliver <oliver@archer.fi>
2021-02-15 12:29:40 +0000

    Added crates for parsing token streams. Added a structure to store parsed data from syn with the syn Parse trait implemented, however it does not parse anything currently

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,27 @@
 use proc_macro::TokenStream;
+use quote::quote;
+use syn::{
+    parse::{Parse, ParseStream},
+    ExprArray, Result,
+};
+
+// Structure storing details about a matrix with the data to be parsed from the tokenstream
+#[derive(Debug)]
+struct MatrixInput {
+    // Token Stream of array
+    parsed_arr: proc_macro2::TokenStream,
+    dimensions: (usize, usize),
+}
+
+impl Parse for MatrixInput {
+    fn parse(input: ParseStream) -> Result<Self> {
+        let _array = ExprArray::parse(input)?;
+        Ok(MatrixInput {
+            parsed_arr: quote! {#[0; 10]},
+            dimensions: (0, 0),
+        })
+    }
+}
 
 #[proc_macro]
 pub fn matrix(_input: TokenStream) -> TokenStream {
