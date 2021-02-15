02b4f9bbbe5bf1ef91fd4faad5ad65502b2890c4
Oliver <oliver@archer.fi>
2021-02-15 23:09:39 +0000

    Added function to ensure array is 2d. Array parsing is now done

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ use quote::quote;
 use syn::{
     parse::{Parse, ParseStream},
     spanned::Spanned,
-    Error, Expr, ExprArray, Result,
+    Error, Expr, ExprArray, Lit, Result,
 };
 
 // Structure storing details about a matrix with the data to be parsed from the tokenstream
@@ -18,8 +18,9 @@ impl Parse for MatrixInput {
     fn parse(input: ParseStream) -> Result<Self> {
         let array = ExprArray::parse(input)?;
         let dimensions = check_array_length(&array)?;
+        let parsed_arr = check_valid_input(&array)?;
         Ok(MatrixInput {
-            parsed_arr: quote! {#[0; 10]},
+            parsed_arr,
             dimensions,
         })
     }
@@ -53,6 +54,39 @@ fn check_array_length(array: &ExprArray) -> Result<(usize, usize)> {
         return Ok((array.elems.len(), sub_array.elems.len()));
     }
     Err(Error::new(array.span(), "Expected a 2d array"))
+}
+
+// Iterates through the passed in 2d array ensuring that it is only a 2d array.
+// Creates a token stream of a flattened array of the passed in 2d array
+fn check_valid_input(array: &ExprArray) -> Result<proc_macro2::TokenStream> {
+    let mut arr_out = proc_macro2::TokenStream::new();
+    for sub_array in &array.elems {
+        if let Expr::Array(sub_array) = sub_array {
+            for elem in &sub_array.elems {
+                if let Expr::Lit(lit) = elem {
+                    if let Lit::Int(lit) = &lit.lit {
+                        if arr_out.is_empty() {
+                            arr_out = quote! { #lit }
+                        } else {
+                            arr_out = quote! { #arr_out, #lit }
+                        }
+                    } else {
+                        return Err(Error::new(lit.lit.span(), "Expected int or float."));
+                    };
+                } else if let Expr::Array(arr) = elem {
+                    return Err(Error::new(arr.span(), "Expected a 2d array"));
+                } else {
+                    return Err(Error::new(
+                        elem.span(),
+                        "Expected integer or float literal.",
+                    ));
+                }
+            }
+        } else {
+            return Err(Error::new(sub_array.span(), "Expected a 2d array."));
+        }
+    }
+    Ok(quote! {[#arr_out]})
 }
 
 #[proc_macro]
