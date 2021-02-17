20a40f3d6b6cd723ab5c76c720b99e1516026ba5
Oliver <oliver@archer.fi>
2021-02-17 12:01:34 +0000

    Added indexing to matrix struct as well as fixing bug which meant that the x and y dimensions of the struct were inverted

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 mod parse;
 
 use proc_macro::TokenStream;
+use proc_macro2::{Ident, Span};
 use quote::quote;
 use syn::{
     parse::{Parse, ParseStream},
@@ -33,28 +34,44 @@ pub fn matrix(input: TokenStream) -> TokenStream {
     let output_len = input.dimensions.0 * input.dimensions.1;
     let output_arr = input.parsed_arr;
 
-    let x_dim = input.dimensions.0;
-    let y_dim = input.dimensions.1;
+    let x_dim = input.dimensions.1;
+    let y_dim = input.dimensions.0;
+
+    let matrix = Ident::new(&format!("Matrix{}x{}", x_dim, y_dim), Span::call_site());
 
     TokenStream::from(quote! {{
         use std::fmt::{Display, Formatter, Result};
-        #[derive(Debug)]
-        pub struct Matrix([i32; #output_len]);
+        #[derive(Debug, Eq, PartialEq)]
+        pub struct #matrix([i32; #output_len]);
 
         // Prints the user inputted array in the same dimensions as the passed in
-        impl Display for Matrix {
+        impl Display for #matrix {
             fn fmt(&self, f: &mut Formatter<'_>) -> Result {
-                for i in 0..#x_dim {
+                for i in 0..#y_dim {
                     if i == 0 { write!(f, "[")? } else { write!(f, " ")? };
-                    for j in 0..#y_dim {
-                        write!(f, "{:>3},", self.0[#y_dim*i+j])?;
+                    for j in 0..#x_dim {
+                        write!(f, "{:>3},", self.0[#x_dim*i+j])?;
                     }
-                    if i == #x_dim-1 { write!(f, "]")? } else { write!(f, "\n")? };
+                    if i == #y_dim-1 { write!(f, "]")? } else { write!(f, "\n")? };
                 }
                 Ok(())
             }
         }
 
-        Matrix(#output_arr)
+        impl<U> std::ops::Index<[U; 2]> for #matrix
+        where
+            U: std::convert::Into<usize> + std::marker::Copy,
+            usize: std::ops::Mul<U, Output = usize> + std::ops::Add<U, Output = usize>,
+        {
+            // TODO: This has to be made more generic if the rest of the macro is made more generic
+            type Output = i32;
+
+            fn index(&self, idx: [U; 2]) -> &Self::Output {
+                let index = #x_dim * idx[1] + idx[0];
+                &self.0[index]
+            }
+        }
+
+        #matrix(#output_arr)
     }})
 }
