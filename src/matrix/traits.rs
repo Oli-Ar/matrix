524c8429bb11a0179b7d1f6ee71950ae3bbf4a35
Oliver <oliver@archer.fi>
2021-02-20 14:40:18 +0000

    Implemented indexing NOTE: this indexing can panic at run time so I'll implement indexing which returns a result type

--- src/matrix/traits.rs
+++ src/matrix/traits.rs
@@ -1,5 +1,6 @@
-use super::*;
+use super::{Data, Matrix};
 use std::fmt::{Display, Formatter};
+use std::ops::Index;
 
 impl<T: Data, const N: usize> Display for Matrix<T, { N }> {
     // Prints a two dimensional representaion of the matrix
@@ -11,5 +12,16 @@ impl<T: Data, const N: usize> Display for Matrix<T, { N }> {
             writeln!(f)?;
         }
         Ok(())
+    }
+}
+
+impl<T: Data, const N: usize> Index<[usize; 2]> for Matrix<T, { N }> {
+    type Output = T;
+    fn index(&self, index: [usize; 2]) -> &Self::Output {
+        if index[0] >= self.x || index[1] >= self.y {
+            // TODO: Write better error message
+            panic!("Index out of bounds.");
+        }
+        &self.dat[index[1] * self.x + index[0]]
     }
 }
