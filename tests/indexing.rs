53ad7d95b0827dbffc8f2eb8ce347783a4f45184
Oliver <oliver@archer.fi>
2021-02-20 14:34:46 +0000

    Improved tests to test for error cases

--- tests/indexing.rs
+++ tests/indexing.rs
@@ -10,5 +10,21 @@
 //         Matrix::<i32, LENGTH>::new([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15]]);
 //     assert_eq!(matrix[[0, 2]], 11);
 //     assert_eq!(matrix[[0, 0]], 1);
-//     assert_eq!(matrix[[1, 4]], 10);
+//     assert_eq!(matrix[[4, 1]], 10);
+// }
+//
+// #[test]
+// #[should_panic(expected = "Index out of bounds")]
+// fn index_x_out_of_bounds() {
+//     let matrix =
+//         Matrix::<i32, LENGTH>::new([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15]]);
+//     matrix[[5, 2]];
+// }
+//
+// #[test]
+// #[should_panic(expected = "Index out of bounds")]
+// fn index_y_out_of_bounds() {
+//     let matrix =
+//         Matrix::<i32, LENGTH>::new([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15]]);
+//     matrix[[3, 4]];
 // }
