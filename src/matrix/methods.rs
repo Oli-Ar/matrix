434e3bbfed0c0c5b763cffc52b0bc4f200441ac2
Oliver <oliver@archer.fi>
2021-07-03 23:21:43 +0100

    Added methods to return raw data and 2D array representation of matrix
    Implemented methods which return either the raw data of the matrix, a
    one dimensional array, or a 2d representation of this data, this is so
    the dat field doesn't have to be public.

--- src/matrix/methods.rs
+++ src/matrix/methods.rs
@@ -1,4 +1,5 @@
 use super::{Data, Matrix};
+use std::mem::transmute;
 
 impl<D: Data, const M: usize, const N: usize> Matrix<D, M, N>
 where
@@ -12,7 +13,7 @@ where
         // transmuting to flatten it is safe
         // This is done so the new function is const and doesn't required a for loop to copy the
         // data into the flattened array
-        let dat: &[D; M * N] = unsafe { std::mem::transmute::<&[[D; N]; M], &[D; M * N]>(&data) };
+        let dat: &[D; M * N] = unsafe { transmute::<&[[D; N]; M], &[D; M * N]>(&data) };
         Matrix { dat: *dat }
     }
 
@@ -21,5 +22,17 @@ where
     // passed
     pub const fn vector(dat: [D; M * N]) -> Self {
         Matrix { dat }
+    }
+
+    // For getting the raw data stored in the matrix struct
+    pub const fn raw(self) -> [D; M * N] {
+        self.dat
+    }
+
+    // Returns the data stored in the matrix struct in 2D array form
+    // Same safety applied as when creating an array from a 2d array as seen in the new method -
+    // this should be fine to do
+    pub const fn data(self) -> [[D; N]; M] {
+        *unsafe { transmute::<&[D; M * N], &[[D; N]; M]>(&self.dat) }
     }
 }
