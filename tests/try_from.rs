b3f424cb0eaea00990cc28b7d399ab79f4c3d852
Oliver <oliver@archer.fi>
2021-07-03 21:53:52 +0100

    Removed try_from implementaion and test

--- tests/try_from.rs
+++
@@ -1,8 +0,0 @@
-// #[test]
-// fn from() {
-//     let matrix1 = Matrix::try_from([[1, 2, 3], [4, 5, 6], [7, 8, 9]]);
-//     assert!(matrix1.is_ok());
-//
-//     let matrix2 = Matrix::try_from([[1, 2, 3, 4], [5, 6, 7, 8, 9]]);
-//     assert_eq!(matrix2, Err(MtxErr::InvalidInput));
-// }
