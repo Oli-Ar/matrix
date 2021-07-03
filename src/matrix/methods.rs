c7b82c7a26716893cffbff70a5850f02fc97ae14
Oliver <oliver@archer.fi>
2021-07-03 22:02:43 +0100

    Updated matrix new method to work with updated matrix strct and to make flattening the input a const function.
    The unsafe code in the function will not result in UB due to Rust
    guaranteeing that an array will not have padding and have the elements
    in the same order in memory, this means using transmute will not result
    in UB.

--- src/matrix/methods.rs
+++ src/matrix/methods.rs
@@ -1,23 +1,18 @@
 use super::{Data, Matrix};
 
-impl<D: Data, const N: usize> Matrix<D, { N }> {
-    // Result return is not necessary as rust will require the matrix to be X by Y at compile time
-    pub fn new<const X: usize, const Y: usize>(data: [[D; X]; Y]) -> Self {
-        // Fill array with whatever is at index 0, 0 of the passed in matrix, the value doens't
-        // matter as it is overwritten
-        let mut dat: [D; N] = [data[0][0]; N];
-        for i in 0..dat.len() {
-            dat[i] = data[i / X][i % X];
-        }
-        Matrix { dat, x: X, y: Y }
+impl<D: Data, const M: usize, const N: usize> Matrix<D, M, N>
+where
+    D: From<i32>,
+    [D; M * N]: Sized,
+{
+    // Compiler will assets that passed in array is correct size so returing a result is not
+    // required
+    pub const fn new(data: [[D; N]; M]) -> Self {
+        // This should be safe as nested arrays are guaranteed to have no padding or reordering so
+        // transmuting to flatten it is safe
+        // This is done so the new function is const and doesn't required a for loop to copy the
+        // data into the flattened array
+        let dat: &[D; M * N] = unsafe { std::mem::transmute::<&[[D; N]; M], &[D; M * N]>(&data) };
+        Matrix { dat: *dat }
     }
-
-    // To create column vectors without having to pass in [[1], [2], [3], ... , [n]]
-    // Doesn't return a result as the function can't fail as long as the passed arguments can be
-    // passed
-    pub fn vector(dat: [D; N]) -> Self {
-        Matrix { dat, x: 1, y: N }
-    }
-
-    // TODO: Write method to create a random matrix of a given size
 }
