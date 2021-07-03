9c999462d183d31d30bda36a9c476238771ca0b4
Oliver <oliver@archer.fi>
2021-07-03 21:46:15 +0100

    Updated to add more incomplete features at root to use in other parts of the project

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,9 @@
-#![allow(clippy::pedantic)]
+#![allow(clippy::pedantic, incomplete_features)]
 #![feature(trait_alias)]
+#![feature(const_generics)]
+#![feature(const_evaluatable_checked)]
+#![feature(const_fn)]
+#![feature(const_fn_transmute)]
+#![feature(const_refs_to_cell)]
+
 pub mod matrix;
