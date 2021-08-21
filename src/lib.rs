6324d4e4730bdc4c76a706a93bb500555ab4f691
Oliver Archer <oliver@archer.fi>
2021-08-21 22:14:32 +0100

    Updated nightly features
    Compiler was updated so new features had to be added, and removed, so the same code
    could run

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,8 @@
 #![feature(trait_alias)]
 #![feature(const_generics)]
 #![feature(const_evaluatable_checked)]
-#![feature(const_fn)]
 #![feature(const_fn_transmute)]
+#![feature(const_fn_trait_bound)]
 #![feature(const_refs_to_cell)]
 
 pub mod matrix;
