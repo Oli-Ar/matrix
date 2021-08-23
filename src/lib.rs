9a4e36d53960ee3f941283de66a83271a54e349a
Oliver Archer <oliver@archer.fi>
2021-08-23 23:00:06 +0100

    Removed unnecessary unstable feature
    Transmute in constant functions was stabilised so no longer had to be
    added as a feature

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,6 @@
 #![feature(trait_alias)]
 #![feature(const_generics)]
 #![feature(const_evaluatable_checked)]
-#![feature(const_fn_transmute)]
 #![feature(const_fn_trait_bound)]
 #![feature(const_refs_to_cell)]
 
