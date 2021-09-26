a1fc05b88ac7c7e572913c1da1e9ad7e68120c5d
Oliver <oliver@archer.fi>
2021-09-26 21:09:53 +0100

    Test C lib to learn how to import C libs
    Test C function to be built with meson and then imported into rust,
    later will add CUDA code which will also then be built and imported as a
    library into rust.

---
+++ CUDA/test.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "test.h"
+
+void print_num(int num) {
+    printf("The number is %d", num);
+}
---
+++ CUDA/test.h
@@ -0,0 +1,18 @@
+//
+// Created by oliver on 25/09/2021.
+//
+
+#ifndef TEST_H
+#define TEST_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void print_num(int num);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif //TEST_H
