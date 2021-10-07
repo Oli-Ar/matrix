2ca783ab975ccb458aa4178de204ca4f773a658a
Oliver Archer <oliver@archer.fi>
2021-10-07 13:05:18 +0100

    Implemented Default, Index, IndexMut, and Display traits
    Implement the Default, Index, IndexMut, and Display trait to the Heap
    matrix. The default implementation defaults the order of the matrix to
    3x3 but this can be overwridden as usual wit the default trait. The
    Display trait implementation is the same as for stack matrix

--- src/heap/utils.rs
+++ src/heap/utils.rs
@@ -1,5 +1,6 @@
 use super::HeapMatrix;
-use alloc::vec::Vec;
+use std::fmt::{self, Debug, Display, Formatter};
+use std::vec::Vec;
 
 impl<D: Clone> HeapMatrix<D> {
     // TODO: This function is shit, rewrite it later
@@ -9,7 +10,7 @@ impl<D: Clone> HeapMatrix<D> {
             panic!("All rows of matrix must have same amount of collumns")
         }
         HeapMatrix {
-            dat: data
+            buf: data
                 .iter()
                 .fold(Vec::with_capacity(data.len() * cols), |mut a, n| {
                     a.append(&mut n.to_vec());
@@ -22,17 +23,55 @@ impl<D: Clone> HeapMatrix<D> {
 
     #[inline(always)]
     pub fn raw(self) -> Vec<D> {
-        self.dat
+        self.buf
     }
 
     // These lifetimes work, don't touch
     #[inline(always)]
     pub fn data<'a>(&'a self) -> Vec<&'a [D]> {
-        self.dat.chunks(self.cols).collect::<Vec<&'a [D]>>()
+        self.buf.chunks(self.cols).collect::<Vec<&'a [D]>>()
     }
 
     #[inline(always)]
     pub fn size(self) -> (usize, usize) {
         (self.cols, self.rows)
+    }
+}
+
+impl<T: Clone + Default> Default for HeapMatrix<T> {
+    fn default() -> Self {
+        let mut tmp = HeapMatrix {
+            buf: Vec::new(),
+            rows: 3,
+            cols: 3,
+        };
+        tmp.buf = std::vec![Default::default(); tmp.rows * tmp.cols];
+        tmp
+    }
+}
+
+impl<T: Clone> std::ops::Index<[usize; 2]> for HeapMatrix<T> {
+    type Output = T;
+    fn index(&self, index: [usize; 2]) -> &T {
+        &self.buf[self.cols * index[0] + index[1]]
+    }
+}
+
+impl<T: Clone> std::ops::IndexMut<[usize; 2]> for HeapMatrix<T> {
+    fn index_mut(&mut self, index: [usize; 2]) -> &mut T {
+        &mut self.buf[self.cols * index[0] + index[1]]
+    }
+}
+
+impl<T: Debug> Display for HeapMatrix<T> {
+    // Prints a two dimensional representaion of the matrix
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        for row in 0..self.cols {
+            for col in 0..self.rows {
+                write!(f, "{:>2?},", self.buf[self.cols * row + col])?;
+            }
+            writeln!(f)?;
+        }
+        Ok(())
     }
 }
